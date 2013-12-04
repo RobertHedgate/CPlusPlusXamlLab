7ea33289f2a4ecea50c262fe215544d72b1737ce
RobertHedgate <robert.hedgate@jayway.com>
2013-12-04 16:07:19 +0100

    project created

---
+++ C++XamlLab/SpeedCSharp/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace SpeedCSharp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    {
+        private double _time;
+
+        public double Time
+        {
+            get { return _time; }
+            set
+            {
+                _time = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainPage()
+        {
+            this.InitializeComponent();
+            DataContext = this;
+        }
+
+        private void StartClicked(object sender, RoutedEventArgs e)
+        {
+            var firstValue = 1.0;
+            var result = 1.0;
+
+            var timeBefore = DateTime.Now;
+
+            for (var i = 0; i < 1000000; i++)
+            {
+                var oldResult = result;
+                result = firstValue + result;
+                firstValue = oldResult;
+            }
+
+            var timeAfter = DateTime.Now;
+            Time = (timeAfter - timeBefore).Milliseconds;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
